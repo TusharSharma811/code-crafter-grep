9f55b1a4444ca09a088e5b65cd7691620c8c1b7b
Tushar Sharma <sharmatushar811@gmail.com>
2024-08-24 22:18:43 +0530

    Sixth stage

--- src/Server.cpp
+++ src/Server.cpp
@@ -17,6 +17,14 @@ bool match_pattern(const std::string& input_line, const std::string& pattern) {
         }
         return false;
     }
+    else if (pattern.at(0) == '[' && pattern.at(1) == '^' && pattern.at(pattern.length() - 1) == ']') {
+        for (const auto &l : pattern.substr(2, pattern.length() - 2)) {
+            if (input_line.find(l) != std::string::npos) {
+                return false;
+            }
+        }
+        return true;
+    }
     else if (pattern.at(0) == '[' && pattern.at(pattern.length() - 1) == ']') {
         for (const auto &l : pattern.substr(1, pattern.length() - 2)) {
             if (input_line.find(l) != std::string::npos) {
